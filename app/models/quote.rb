3e060b975c636ccee19219014ffa151c2386a4d4
Alex Schwartz <aschwartz@detaso.com>
2022-09-09 08:31:13 -0500

    Add company database references

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,6 +1,8 @@
 class Quote < ApplicationRecord
   validates :name, presence: true
 
+  belongs_to :company
+
   scope :ordered, -> { order(id: :desc) }
 
   broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
