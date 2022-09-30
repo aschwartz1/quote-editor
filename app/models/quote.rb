a6cab5aabaa38bdb3481a06b28c1dbed1c3bbd59
Alex Schwartz <aschwartz@detaso.com>
2022-09-30 08:34:54 -0500

    Add and basic setup for LineItemDate

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,6 +3,8 @@ class Quote < ApplicationRecord
 
   belongs_to :company
 
+  has_many :line_item_dates, dependent: :destroy
+
   scope :ordered, -> { order(id: :desc) }
 
   broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
