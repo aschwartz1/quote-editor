600d120776ef3b40e42111edaa2a9d11d3f4df2b
Alex Schwartz <aschwartz@detaso.com>
2022-10-13 10:12:12 -0500

    Refactor to create `nested_dom_id` helper
    This provides a standardized way to generate dom ids for nested
    resources.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,19 @@
 module ApplicationHelper
-  def render_turbo_stream_flash_messages
-    turbo_stream.prepend "flash", partial: "layouts/flash"
-  end
-
   def form_error_notification(object)
     if object.errors.any?
       tag.div class: "error-message" do
         object.errors.full_messages.to_sentence.capitalize
       end
     end
+  end
+
+  def nested_dom_id(*args)
+    args.map do |arg|
+      arg.respond_to?(:to_key) ? dom_id(arg) : arg
+    end.join("_")
+  end
+
+  def render_turbo_stream_flash_messages
+    turbo_stream.prepend "flash", partial: "layouts/flash"
   end
 end
